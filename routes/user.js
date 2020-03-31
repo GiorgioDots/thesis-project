d6b63e07b02bf119286ee505ff351d2dc0e2c2dc
GioDots <dodedodo96@gmail.com>
2020-03-31 21:14:27 +0200

    removed useless import

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { body } = require("express-validator");
 
-const User = require("../models/user");
 const userController = require("../controllers/user");
 const isAuth = require("../middleware/is-auth");
 
