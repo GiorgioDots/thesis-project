f73b27cca040c26cf5e8b05632f8308c3aec0ae6
GioDots <dodedodo96@gmail.com>
2020-03-31 21:09:32 +0200

    user settings apis improved

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const { body } = require('express-validator');
+const express = require("express");
+const { body } = require("express-validator");
 
-const User = require('../models/user');
-const userController = require('../controllers/user');
-const isAuth = require('../middleware/is-auth');
+const User = require("../models/user");
+const userController = require("../controllers/user");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 router.put(
-    '/:id',
-    [
-        body('telegramId')
-            .trim(),
-        body('name')
-            .trim()
-    ],
-    isAuth,
-    userController.updateUser
+  "/",
+  [
+    body("telegramId")
+      .optional()
+      .trim(),
+    body("name")
+      .optional()
+      .trim(),
+    body("password", "Password must be min 5 characters long")
+      .optional()
+      .trim()
+      .isLength({ min: 5 }),
+    body("email", "Please enter a valid e-mail.")
+      .optional()
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+  ],
+  isAuth,
+  userController.updateUser
 );
 
 module.exports = router;
